d773834dadd4449bdbb15eba7d755fa5c3d7a612
vigkast <vigneshkasthuri2009@gmail.com>
2015-07-16 11:48:18 +0530

    image resize changes

--- api/controllers/UserController.js
+++ api/controllers/UserController.js
@@ -13,7 +13,6 @@ module.exports = {
                 var source = sails.fs.createReadStream(n.fd);
                 n.fd = n.fd.split("\\").pop();
                 var dest = sails.fs.createWriteStream('./uploads/' + n.fd);
-
                 source.pipe(dest);
                 source.on('end', function () {
                     sails.fs.unlink(oldpath, function (data) {
@@ -30,13 +29,67 @@ module.exports = {
             });
         });
     },
+    //    resize2: function (req, res) {
+    //        sails.im.resize({
+    //            srcData: fs.readFileSync('./uploads/demo.png', 'binary'),
+    //            width: 1000,
+    //            height: 10000
+    //        }, function (err, stdout, stderr) {
+    //            sails.fs.writeFileSync('./uploads/kittens-resized.jpg', stdout, 'binary');
+    //            console.log('resized kittens.jpg to fit within 256x256px')
+    //        });
+    //    },
     resize: function (req, res) {
-        var file = req.query.file;
-        var image = sails.fs.readFileSync("./uploads/" + file);
-        res.set('Content-Type', 'image/png');
-        if (req.query.width == "") {
-            
+        function showimage(path) {
+            var image = sails.fs.readFileSync(path);
+            res.set('Content-Type', 'image/png');
             res.send(image);
+        }
+
+        function checknewfile(newfilepath, width, heigth) {
+            newfilenamearr = newfilepath.split(".");
+            extension = newfilname.pop();
+            var newfilename = "";
+            _.each(newfilename, function (n) {
+                newfilename += n;
+            });
+            newfilename += "_" + width + "_" + height + extension;
+            var isfile2 = fs.existsSync(newfilename);
+            if (!isfile2) {
+
+
+            } else {
+                showimage(newfilename);
+            }
+        }
+
+        var file = req.query.file;
+        var filepath = './uploads/' + file;
+        var newheight = req.query.height;
+        var newwidth = req.query.width;
+
+
+        var isfile = fs.existsSync(filepath);
+        if (!isfile) {
+            res.json({
+                message: "File not found",
+                value: "false"
+            });
+        } else {
+
+            if (!newwidth && !newheight) {
+                showimage(filepath);
+            } else if (!newwidth && newheight) {
+                newheight = parseInt(newheight);
+                newwidth = dimensions.width / dimensions.height * newheight;
+                checknewfile(filepath, newwidth, newheight);
+            } else if (newwidth && !newheight) {
+                newwidth = parseInt(newwidth);
+                newheight = dimensions.height / dimensions.width * newwidth;
+                checknewfile(filepath, newwidth, newheight);
+            } else {
+                checknewfile(filepath, newwidth, newheight);
+            }
         }
     },
     save: function (req, res) {
