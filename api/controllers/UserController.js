1e68dfdc785266762371231ecc16160bc315e951
vigkast <vigneshkasthuri2009@gmail.com>
2015-08-12 16:03:44 +0530

    some ore'

--- api/controllers/UserController.js
+++ api/controllers/UserController.js
@@ -11,7 +11,7 @@ var json2xls = require('json2xls');
 var app = express();
 module.exports = {
     uploadfile: function (req, res) {
-         sails.query(function (err, db) {
+        sails.query(function (err, db) {
             if (err) {
                 console.log(err);
             }
@@ -161,89 +161,6 @@ module.exports = {
                         console.log(err);
                     }
                     res.set('Content-Type', file.contentType);
-                    var stream = file.stream();
-                    stream.pipe(res);
-                });
-            });
-        }
-    },
-        var file = req.param('file');
-        var fd = sails.ObjectID(file);
-        var newheight = req.param('height');
-        var newwidth = req.param('width');
-        if (!newwidth && !newheight) {
-            showimage(fd);
-        } else if (!newwidth && newheight) {
-            newheight = parseInt(newheight);
-            findimage(fd, 0, newheight);
-        } else if (newwidth && !newheight) {
-            newwidth = parseInt(newwidth);
-            findimage(fd, newwidth, 0);
-        } else {
-            findimage(fd, newwidth, newheight);
-        }
-
-        function findimage(fd, newwidth, newheight) {
-            sails.query(function (err, db) {
-                if (err) {
-                    console.log(err);
-                }
-                sails.GridStore.read(db, fd, function (err, fileData) {
-                    width = parseInt(newwidth);
-                    height = parseInt(newheight);
-                    lwip.open(fileData, 'jpg', function (err, image) {
-                        var dimensions = {};
-                        dimensions.width = image.width();
-                        dimensions.height = image.height();
-                        if (width == 0) {
-                            width = dimensions.width / dimensions.height * height;
-                        }
-                        if (height == 0) {
-                            height = dimensions.height / dimensions.width * width;
-                        }
-                        if (err) {
-                            console.log(err);
-                        }
-                        image.resize(width, height, "lanczos", function (err, image2) {
-                            db.open(function (err, db) {
-                                var fileId = new sails.ObjectID();
-                                var gridStore = new sails.GridStore(db, fileId, 'w');
-                                gridStore.open(function (err, gridStore) {
-                                    if (err) {
-                                        console.log(err);
-                                    }
-                                    image2.toBuffer("jpg", {}, function (err, imagebuf) {
-                                        gridStore.write(imagebuf, function (err, doc) {
-                                            if (err) {
-                                                console.log(err);
-                                            }
-                                            if (doc) {
-                                                gridStore.close(function () {
-                                                    showimage(fileId);
-                                                });
-                                            }
-                                        });
-                                    });
-                                });
-                            });
-                        });
-                    });
-                });
-            });
-        }
-
-        function showimage(oldfile) {
-            sails.query(function (err, db) {
-                if (err) {
-                    console.log(err);
-                }
-                var filename = oldfile;
-                var file = new sails.GridStore(db, filename, "r");
-                file.open(function (err, file) {
-                    if (err) {
-                        console.log(err);
-                    }
-                    res.set('Content-Type', 'image');
                     var stream = file.stream();
                     stream.pipe(res);
                 });
