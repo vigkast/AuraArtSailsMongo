99552aa46ef43a3f251bf43bc6542a1b351057bf
Chintan Shah <chintan@wohlig.com>
2015-09-07 11:23:47 +0530

    dbconsokle

--- api/controllers/UserController.js
+++ api/controllers/UserController.js
@@ -272,7 +272,7 @@ module.exports = {
                     console.log(err);
                 }
                 var filename = oldfile;
-                console.log(filename);
+                console.log(db);
                 db.open(function (err, database) {
                     if (err) {
                         console.log(err);
