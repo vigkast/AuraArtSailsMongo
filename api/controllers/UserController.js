d16b8b95bb149d797951f3260d2e30d007d2bbb9
vigkast <vigwohlig@gmail.com>
2015-09-08 13:44:19 +0530

    Revert "sails query merged"
    This reverts commit f5b05e7ec058dfc49e637e2082c80e8f8136a35f.

--- api/controllers/UserController.js
+++ api/controllers/UserController.js
@@ -207,66 +207,79 @@ module.exports = {
         } else {
             findimage(fd, newwidth, newheight);
         }
-        sails.query(function (err, db) {
-            if (err) {
-                console.log(err);
-                res.json({
-                    value: "busy"
-                });
-            } else {
 
-                function findimage(fd, newwidth, newheight) {
-                    if (database) {
-                        sails.GridStore.read(database, fd, function (err, fileData) {
-                            if (err) {
-                                console.log(err);
-                            }
-                            if (fileData) {
-                                width = parseInt(newwidth);
-                                height = parseInt(newheight);
-                                sails.lwip.open(fileData, 'jpg', function (err, image) {
-                                    if (err) {
-                                        console.log(err);
-                                    }
-                                    if (image) {
-                                        var dimensions = {};
-                                        dimensions.width = image.width();
-                                        dimensions.height = image.height();
-                                        if (width == 0) {
-                                            width = dimensions.width / dimensions.height * height;
+        function findimage(fd, newwidth, newheight) {
+            sails.query(function (err, db) {
+                if (err) {
+                    console.log(err);
+                    res.json({
+                        value: "busy"
+                    });
+                } else if (db) {
+                    db.open(function (err, database) {
+                        if (err) {
+                            console.log(err);
+                        }
+                        if (database) {
+                            sails.GridStore.read(database, fd, function (err, fileData) {
+                                if (err) {
+                                    console.log(err);
+                                }
+                                if (fileData) {
+                                    width = parseInt(newwidth);
+                                    height = parseInt(newheight);
+                                    sails.lwip.open(fileData, 'jpg', function (err, image) {
+                                        if (err) {
+                                            console.log(err);
                                         }
-                                        if (height == 0) {
-                                            height = dimensions.height / dimensions.width * width;
+                                        if (image) {
+                                            var dimensions = {};
+                                            dimensions.width = image.width();
+                                            dimensions.height = image.height();
+                                            if (width == 0) {
+                                                width = dimensions.width / dimensions.height * height;
+                                            }
+                                            if (height == 0) {
+                                                height = dimensions.height / dimensions.width * width;
+                                            }
+                                            image.resize(width, height, "lanczos", function (err, image2) {
+                                                if (err) {
+                                                    console.log(err);
+                                                }
+                                                if (image2) {
+                                                    var fileId = new sails.ObjectID();
+                                                    var mimetype = "image/jpeg";
+                                                    image2.toBuffer("jpg", {}, function (err, imagebuf) {
+                                                        if (err) {
+                                                            console.log(err);
+                                                        }
+                                                        if (imagebuf) {
+                                                            res.set('Content-Type', mimetype);
+                                                            res.send(new Buffer(imagebuf));
+                                                            db.close();
+                                                        }
+                                                    });
+                                                }
+                                            });
                                         }
-                                        image.resize(width, height, "lanczos", function (err, image2) {
-                                            if (err) {
-                                                console.log(err);
-                                            }
-                                            if (image2) {
-                                                var fileId = new sails.ObjectID();
-                                                var mimetype = "image/jpeg";
-                                                image2.toBuffer("jpg", {}, function (err, imagebuf) {
-                                                    if (err) {
-                                                        console.log(err);
-                                                    }
-                                                    if (imagebuf) {
-                                                        res.set('Content-Type', mimetype);
-                                                        res.send(new Buffer(imagebuf));
-                                                        db.close();
-                                                    }
-                                                });
-                                            }
-                                        });
-                                    }
-                                });
-                            }
-                        });
-                    }
+                                    });
+                                }
+                            });
+                        }
+                    });
                 }
+            });
+        }
 
-                function showimage(oldfile) {
-                    var filename = oldfile;
-
+        function showimage(oldfile) {
+            var filename = oldfile;
+            sails.query(function (err, db) {
+                if (err) {
+                    console.log(err);
+                    res.json({
+                        value: "busy"
+                    });
+                } else if (db) {
                     db.open(function (err, database) {
                         if (err) {
                             console.log(err);
@@ -285,8 +298,8 @@ module.exports = {
                         }
                     });
                 }
-            }
-        });
+            });
+        }
     },
     findimage: function (req, res) {
         var print = function (data) {
