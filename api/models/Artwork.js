fbdd032dfe89f2c6a89ef837742247baa5745c27
vigkast <vigwohlig@gmail.com>
2015-09-24 10:47:04 +0530

    changes

--- api/models/Artwork.js
+++ api/models/Artwork.js
@@ -870,7 +870,7 @@ module.exports = {
                         callbackfunc1();
                     }
                 } else if (data.type == "Sculptures") {
-                    if (data.minwidth == 0 && data.maxwidth == 10000 && data.minprice == 0 && data.maxprice == 10000000 && data.minheight == 0 && data.maxheight == 10000) {
+                    if (data.minwidth == 0 && data.maxwidth == 10000 && data.minprice == 0 && data.maxprice == 10000000 && data.minheight == 0 && data.maxheight == 10000 && data.minbreadth == 0 && data.maxbreadth == 10000) {
                         var matchobj = {
                             "name": {
                                 $regex: check
