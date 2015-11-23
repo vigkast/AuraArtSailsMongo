fd27b1b14737009e76037a571b49dd5f8e345c79
vigkast <vigwohlig@gmail.com>
2015-11-23 18:14:35 +0530

    artwork

--- api/controllers/WishlistController.js
+++ api/controllers/WishlistController.js
@@ -44,7 +44,7 @@ module.exports = {
     delete: function(req, res) {
         if (req.body) {
             if (req.body.user && req.body.user != "" && sails.ObjectID.isValid(req.body.user)) {
-                if (req.body._id && req.body._id != "" && sails.ObjectID.isValid(req.body._id)) {
+                if (req.body.artwork && req.body.artwork != "" && sails.ObjectID.isValid(req.body.artwork)) {
                     var print = function(data) {
                         res.json(data);
                     }
@@ -52,7 +52,7 @@ module.exports = {
                 } else {
                     res.json({
                         value: "false",
-                        comment: "Wishlist-id is incorrect"
+                        comment: "Artwork-id is incorrect"
                     });
                 }
             } else {
