60e86a4c122caf113b7cbb31ed3e3d81d27d97f9
vigkast <vigwohlig@gmail.com>
2015-12-12 12:34:05 +0530

    changes in wihslist

--- api/models/Wishlist.js
+++ api/models/Wishlist.js
@@ -181,11 +181,12 @@ module.exports = {
                     $unwind: "$wishlist"
                 }, {
                     $project: {
+                        _id:0,
                         wishlist: 1
                     }
                 }]).toArray(function(err, data2) {
-                    if (data2 && data2[0]) {
-                        callback(data2);
+                    if (data2 && data2[0] && data2[0].wishlist && data2[0].wishlist[0]) {
+                        callback(data2[0].wishlist);
                         db.close();
                     } else if (err) {
                         console.log(err);
