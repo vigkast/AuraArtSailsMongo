5ddf25fa56ebcb9621eb752ad61d0bc0e61031d8
dhavalgala <dhaval.gala59@gmail.com>
2016-04-11 18:05:32 +0530

    move background

--- assets/w/w.js
+++ assets/w/w.js
@@ -24126,7 +24126,6 @@ angular.module('phonecatControllers', ['templateservicemod', 'navigationservice'
     $scope.grid = [];
     $scope.grid.status = true;
     $scope.getTimes = function(n) {
-        console.log("getTimes : " + n);
         n = Math.ceil(n);
         return new Array(n);
     };
@@ -24147,6 +24146,123 @@ angular.module('phonecatControllers', ['templateservicemod', 'navigationservice'
         $scope.calcCount();
     }
     $scope.calcCount();
+
+    var AttachDragTo = (function() {
+        var _AttachDragTo = function(el) {
+            this.el = el;
+            this.mouse_is_down = false;
+
+            this.init();
+        };
+
+        _AttachDragTo.prototype = {
+            onMousemove: function(e) {
+                if (!this.mouse_is_down) return;
+                var tg = e.target,
+                    x = e.clientX,
+                    y = e.clientY;
+
+                tg.style.backgroundPositionX = x - this.origin_x + this.origin_bg_pos_x + 'px';
+                tg.style.backgroundPositionY = y - this.origin_y + this.origin_bg_pos_y + 'px';
+            },
+
+            onMousedown: function(e) {
+                console.log("down");
+                this.mouse_is_down = true;
+                this.origin_x = e.clientX;
+                this.origin_y = e.clientY;
+            },
+
+            onMouseup: function(e) {
+                var tg = e.target,
+                    styles = getComputedStyle(tg);
+
+                this.mouse_is_down = false;
+                this.origin_bg_pos_x = parseInt(styles.getPropertyValue('background-position-x'), 10);
+                this.origin_bg_pos_y = parseInt(styles.getPropertyValue('background-position-y'), 10);
+            },
+
+            init: function() {
+                console.log("init");
+                var styles = getComputedStyle(this.el);
+                this.origin_bg_pos_x = parseInt(styles.getPropertyValue('background-position-x'), 10);
+                this.origin_bg_pos_y = parseInt(styles.getPropertyValue('background-position-y'), 10);
+
+                //attach events
+                this.el.addEventListener('mousedown', this.onMousedown.bind(this), false);
+                this.el.addEventListener('mouseup', this.onMouseup.bind(this), false);
+                this.el.addEventListener('mousemove', this.onMousemove.bind(this), false);
+            }
+        };
+
+        return function(el) {
+            new _AttachDragTo(el);
+        };
+    })();
+
+    function startDrag(e) {
+        // determine event object
+        if (!e) {
+            var e = window.event;
+        }
+        if (e.preventDefault) e.preventDefault();
+
+        // IE uses srcElement, others use target
+        targ = e.target ? e.target : e.srcElement;
+
+        if (targ.className != 'painting-holder') {
+            return
+        };
+        // calculate event X, Y coordinates
+        offsetX = e.clientX;
+        offsetY = e.clientY;
+
+        // assign default values for top and left properties
+        if (!targ.style.left) {
+            targ.style.left = '0px'
+        };
+        if (!targ.style.top) {
+            targ.style.top = '0px'
+        };
+
+        // calculate integer values for top and left
+        // properties
+        coordX = parseInt(targ.style.left);
+        coordY = parseInt(targ.style.top);
+        drag = true;
+
+        // move div element
+        document.onmousemove = dragDiv;
+        return false;
+
+    }
+
+    function dragDiv(e) {
+        if (!drag) {
+            return
+        };
+        if (!e) {
+            var e = window.event
+        };
+        // var targ=e.target?e.target:e.srcElement;
+        // move div element
+        targ.style.left = coordX + e.clientX - offsetX + 'px';
+        targ.style.top = coordY + e.clientY - offsetY + 'px';
+        return false;
+    }
+
+    function stopDrag() {
+        drag = false;
+    }
+
+    var map = '';
+    window.onload = function() {
+        console.log("loaded");
+        map = document.getElementById('wall');
+        AttachDragTo(map);
+        document.onmousedown = startDrag;
+        document.onmouseup = stopDrag;
+    }
 
 });
 ;
