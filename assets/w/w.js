3c4d27f84085a18bbd4b5ca2030a2260371ec6a9
dhavalgala <dhaval.gala59@gmail.com>
2016-05-27 15:55:51 +0530

    in lakhs

--- assets/w/w.js
+++ assets/w/w.js
@@ -17991,6 +17991,31 @@ firstapp.filter('indollars', function(NavigationService, $filter) {
     };
 });
 
+firstapp.filter('inlakhs', function(NavigationService, $filter) {
+    return function(input) {
+        if (input) {
+            if (input != "0") {
+                var x = input;
+                x = x.toString();
+                var afterPoint = '';
+                if (x.indexOf('.') > 0)
+                    afterPoint = x.substring(x.indexOf('.'), x.length);
+                x = Math.floor(x);
+                x = x.toString();
+                var lastThree = x.substring(x.length - 3);
+                var otherNumbers = x.substring(0, x.length - 3);
+                if (otherNumbers != '')
+                    lastThree = ',' + lastThree;
+                var res = otherNumbers.replace(/\B(?=(\d{2})+(?!\d))/g, ",") + lastThree + afterPoint;
+                return res;
+            } else
+                return 0;
+        } else {
+            return 0;
+        }
+    };
+});
+
 firstapp.directive('onlyDigits', function() {
     return {
         require: 'ngModel',
